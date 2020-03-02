2be97cd4e6a69c50a7313f35629c93d1fe414808
Numan Hussain <ahnuman5@gmail.com>
2020-03-02 21:26:50 +0600

    Created index file and added author info

---
+++ wp-news-sections.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: NPA Elementor 
+ * Plugin URI: https://github.com/numan-hussain/wp-news-sectionary
+ * Description: News paper all elemenotr plugin is free extension of elementor. You'll get many news paper style sections on this plugin. 
+ * Author: Numan
+ * Author URI: https://www.numanhussain.com
+ * Version: 1.0.0
+ * License: All Rights Reserved*
+ */
+
+  
